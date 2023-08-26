b102337369d2d7aeb3af9aa0da2e22eb9557e095
Jospooa <lirui2@gtmap.cn>
2023-08-26 17:59:33 +0800

    feat: add convertion package

---
+++ convention/db.go
@@ -0,0 +1,10 @@
+package convention
+
+type DBConf struct {
+	Driver   string
+	User     string
+	Password string
+	Host     string
+	Port     uint
+	DBName   string
+}
---
+++ convention/error_code.go
@@ -0,0 +1,30 @@
+package convention
+
+import "fmt"
+
+// 统一错误码
+const (
+	Success      = 0
+	ParamInvalid = 10001 // 无效的参数
+	ParamIsEmpty = 10002
+
+	DataFromDBErr = 20001 // FROM数据库错误
+	DataToDBErr   = 20002 // TO数据库错误
+
+	PortHoldErr      = 30001
+	DoHttpRequestErr = 30002
+	SystemErr        = 40001
+
+	BusinessErr = 50001
+)
+
+type BaseResponse struct {
+	Code int32  `json:"code"`
+	Msg  string `json:"msg"`
+}
+
+var BindErr = &BaseResponse{Code: ParamInvalid, Msg: "无效的参数"}
+
+var EmptyParamErr = func(paramName string) *BaseResponse {
+	return &BaseResponse{Code: ParamIsEmpty, Msg: fmt.Sprintf("参数[%s]不能为空", paramName)}
+}
