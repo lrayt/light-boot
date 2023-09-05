01a7e0b8d8dc8064488a11645ddb5abfae6f4158
lirui <lrayt131129@yeah.net>
2023-09-05 16:08:08 +0800

    feat: 增加获取HttpConf方法

--- kit/http_manager/gin_common.go
+++ kit/http_manager/gin_common.go
@@ -3,6 +3,8 @@ package http_manager
 import (
 	"context"
 	"github.com/gin-gonic/gin"
+	"github.com/lrayt/light-boot/convention"
+	"github.com/lrayt/light-boot/core"
 	"github.com/lrayt/light-boot/core/log_provider"
 )
 
@@ -16,6 +18,30 @@ func ToCTX(c *gin.Context) context.Context {
 	return context.WithValue(context.Background(), log_provider.TraceId, traceId)
 }
 
+//func CORSMiddleware() gin.HandlerFunc {
+//	return func(c *gin.Context) {
+//		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+//		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+//		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With, Signature, X-authorize-uuid")
+//		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+//
+//		if c.Request.Method == "OPTIONS" {
+//			c.AbortWithStatus(200)
+//			return
+//		}
+//
+//		var traceId = c.GetHeader(log_provider.RequestId)
+//
+//		if len(traceId) <= 0 {
+//			traceId = log_provider.NewTraceId()
+//		}
+//		c.Set(log_provider.TraceId, traceId)
+//		c.Writer.Header().Set(log_provider.RequestId, traceId)
+//		c.Next()
+//	}
+//}
+
+// CORSMiddleware 跨域设置
 func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
@@ -28,13 +54,12 @@ func CORSMiddleware() gin.HandlerFunc {
 			return
 		}
 
-		var traceId = c.GetHeader(log_provider.RequestId)
-
-		if len(traceId) <= 0 {
-			traceId = log_provider.NewTraceId()
-		}
-		c.Set(log_provider.TraceId, traceId)
-		c.Writer.Header().Set(log_provider.RequestId, traceId)
 		c.Next()
 	}
+}
+
+func GetHttpConf() (*convention.HttpConf, error) {
+	var httpConf = new(convention.HttpConf)
+	var err = core.GConfigs().PackConf("http", httpConf)
+	return httpConf, err
 }
