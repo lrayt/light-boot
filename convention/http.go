be022ea04578cc092216d5cca628f542fda19a6e
lrayt131129@yeah.net <lrayt@230320S>
2023-09-24 09:31:34 +0800

    feat: 增加静态资源映射

--- convention/http.go
+++ convention/http.go
@@ -1,8 +1,14 @@
 package convention
 
+type StaticMap struct {
+	Route    string
+	FilePath string
+}
+
 type HttpConf struct {
 	Port   uint32
 	UseTLS bool
 	Pem    string
 	Key    string
+	Static []StaticMap
 }
