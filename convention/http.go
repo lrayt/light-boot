4438ac880290ffdd08b56e43793171ae8f432060
lrayt131129@yeah.net <lrayt@230320S>
2023-09-24 10:42:33 +0800

    feat: http获取baseUrl

--- convention/http.go
+++ convention/http.go
@@ -1,14 +1,33 @@
 package convention
 
+import "strconv"
+
 type StaticMap struct {
 	Route    string
 	FilePath string
 }
 
 type HttpConf struct {
+	Host   string
 	Port   uint32
 	UseTLS bool
 	Pem    string
 	Key    string
 	Static []StaticMap
+}
+
+func (c HttpConf) BaseUrl() string {
+	if len(c.Host) <= 0 {
+		c.Host = "127.0.0.1"
+	}
+	var url string
+	if c.UseTLS {
+		url = "https://" + c.Host
+	} else {
+		url = "http://" + c.Host
+	}
+	if c.Port != 80 && c.Port != 443 {
+		url += ":" + strconv.Itoa(int(c.Port))
+	}
+	return url
 }
