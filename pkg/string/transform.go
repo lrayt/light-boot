10af4c76d194fd0ebcb53fde903ea42af3ae5661
lrayt131129@yeah.net <lrayt@230320S>
2023-10-13 22:42:50 +0800

    feat: 增加类型转换方法

---
+++ pkg/string/transform.go
@@ -0,0 +1,16 @@
+package string
+
+import "unsafe"
+
+func Byte2Str(b []byte) string {
+	return *(*string)(unsafe.Pointer(&b))
+}
+
+func Str2Byte(s string) []byte {
+	return *(*[]byte)(unsafe.Pointer(
+		&struct {
+			string
+			Cap int
+		}{s, len(s)},
+	))
+}
