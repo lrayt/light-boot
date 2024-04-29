18e11099084ebe34e753e71113cbac792e84a07e
lirui <lrayt131129@yeah.net>
2024-04-29 13:41:06 +0800

    feat: 增加SchemaName

--- convention/db.go
+++ convention/db.go
@@ -1,10 +1,11 @@
 package convention
 
 type DBConf struct {
-	Driver   string
-	User     string
-	Password string
-	Host     string
-	Port     uint
-	DBName   string
+	Driver     string
+	User       string
+	Password   string
+	Host       string
+	Port       uint
+	DBName     string
+	SchemaName string
 }
